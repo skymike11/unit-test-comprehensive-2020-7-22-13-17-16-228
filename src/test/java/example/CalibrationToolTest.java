87df377c86ed33d99287281723b039e504c0e269
skymike <skymike11@qq.com>
2020-07-23 21:03:36 +0800

    [Guess Number TDD][Sam] test: should_return_false_when_verify_given_11230

--- src/test/java/example/CalibrationToolTest.java
+++ src/test/java/example/CalibrationToolTest.java
@@ -18,4 +18,18 @@ class CalibrationToolTest {
         //then
         assertFalse(result);
     }
+
+    @Test
+    void should_return_false_when_verify_given_11230() {
+        //given
+        int [] inputGuess = {1, 1, 2, 30};
+
+        //when
+        CalibrationTool calibrationTool = new CalibrationTool();
+        boolean result = calibrationTool.verifyInputGuess(inputGuess);
+
+        //then
+        assertFalse(result);
+    }
+
 }
