78bc613dec8968c5a75a15dc7adffca9b71550d9
skymike <skymike11@qq.com>
2020-07-23 20:33:37 +0800

    [Guess Number TDD][Sam] refactor: should_return_1A2B_when_given_answer_1234_and_input_guess_0324  GuessNumber.countAnswerB

--- src/main/java/example/GuessNumber.java
+++ src/main/java/example/GuessNumber.java
@@ -1,5 +1,7 @@
 package example;
 
+import java.util.Arrays;
+
 /**
  * @Auther Sam Li
  * @Date 2020-07-23
@@ -13,13 +15,24 @@ public class GuessNumber {
     }
 
     public String guess(int[] inputGuess) {
-        return String.format("%dA0B", countAnswerA(inputGuess));
+        return String.format("%dA%dB", countAnswerA(inputGuess), countAnswerB(inputGuess));
     }
 
     private int countAnswerA(int[] inputGuess) {
         int correctNumber = 0;
         for (int i = 0; i < inputGuess.length; i++) {
             if (inputGuess[i] == answer[i]) {
+                correctNumber++;
+            }
+        }
+        return correctNumber;
+    }
+
+    private int countAnswerB(int[] inputGuess) {
+        int correctNumber = 0;
+        for (int i = 0; i < inputGuess.length; i++) {
+            int position = Arrays.binarySearch(answer, inputGuess[i]);
+            if (position > 0 && position != i) {
                 correctNumber++;
             }
         }
