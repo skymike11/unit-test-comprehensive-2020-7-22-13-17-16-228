6d806c911bf6be09a08f699674aba65be80bea27
skymike <skymike11@qq.com>
2020-07-27 20:29:05 +0800

    [Guess Number TDD][Sam] refactor: Extract the notification to print the string to the constant class

---
+++ src/main/java/example/Constant.java
@@ -0,0 +1,14 @@
+package example;
+
+/**
+ * @Auther Sam Li
+ * @Date 2020-07-27
+ */
+public class Constant {
+
+    public static final String WELCOME_TIP = "welcome to play, u have 6 chances, u only input 4 number which belong 1 - 9";
+
+    public static final String INPUT_ERR_TIP = "Wrong Input，Input again";
+
+    public static final String END_GAME = "Game Over";
+}
--- src/main/java/example/Launcher.java
+++ src/main/java/example/Launcher.java
@@ -2,29 +2,31 @@ package example;
 
 import java.util.Scanner;
 
+import static example.Constant.*;
+
 /**
  * @Auther Sam Li
  * @Date 2020-07-23
  */
 public class Launcher {
     public void play() {
-        System.out.println("welcome to play, u have 6 chances, u only input 4 number which belong 1 - 9");
+        System.out.println(WELCOME_TIP);
         int times = 6;
         CalibrationTool calibrationTool = new CalibrationTool();
         Scanner scan = new Scanner(System.in);
-        for (int i = times; i > 0; i--) {
+        for (int time = times; time > 0; time--) {
             int[] inputGuess = new int[4];
             for (int j = 0; j < inputGuess.length; j++) {
                 inputGuess[j] = scan.nextInt();
             }
             if (!calibrationTool.isLegalInputGuess(inputGuess)) {
-                System.out.println("Wrong Input，Input again");
+                System.out.println(INPUT_ERR_TIP);
                 break;
             } else {
                 GuessNumber guessNumber = new GuessNumber(new AnswerGeneratorImpl());
                 System.out.println(PrintTool.printAnswerResult(guessNumber.guess(inputGuess)));
             }
         }
-        System.out.println("Game Over");
+        System.out.println(END_GAME);
     }
 }
